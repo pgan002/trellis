53ea447982e756361e7b10d0a55e2433b491e69c
Philip Ganchev <phil.ganchev@gmail.com>
2019-12-16 12:45:34 -0800

    Django integration tests

--- int_to_words/app/tests.py
+++ int_to_words/app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from app.en import TEST_CASES_OK, TEST_CASES_ERROR
+
+
+class TestCalls(TestCase):
+    def test_ok(self):
+        for n, expected_val in TEST_CASES_OK:
+            expected = {'status': 'ok', 'num_in_english': expected_val}
+            response = self.client.get(n)
+            self.assertEquals(response.status_code, 200, n)
+            self.assertEquals(response.body, expected, response.body)
+
+    def test_error(self):
+        for n in TEST_CASES_ERROR:
+            response = self.client.get(n)
+            self.assertEquals(response.status_code, 401, n)
+            self.assertContains(response.body, {'status': 'error'})
