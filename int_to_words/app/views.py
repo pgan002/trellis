91260ae98688acb07b09688a25f899464cba442c
Philip Ganchev <phil.ganchev@gmail.com>
2019-12-15 16:04:33 -0800

    More comments

--- int_to_words/app/views.py
+++ int_to_words/app/views.py
@@ -4,6 +4,16 @@ from app.en import int_to_words
 
 
 def index(request):
+    """
+    Converts an integer represented in digits to words in English.
+    :parameter number (query parameter) - the integer in digits
+    :return If success: Status 200 and a JSON body with key `status` with
+    value "ok" and key `num_in_english` whose value holds the words in
+    English.
+    If the number is invalid, returns status 401 and a JSON body with key
+    `status` with value "error" and key `message` with value explaining the
+    error.
+    """
     n = request.GET.get('number')
     if n is None:
         return JsonResponse(
